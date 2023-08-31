e0c16d8afdf3ec7fcbe513152d609f63e79c4441
ssook <chlthr1222@naver.com>
2023-08-31 16:55:48 +0900

    ADD : custom_card - 끝말잇기 및 짝맞추기 시작 화면이 동일하여 해당 공통 부분 컴포넌트화

--- memory_forest_frontend/lib/widgets/custom_card.dart
+++ memory_forest_frontend/lib/widgets/custom_card.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
-  final String Title;
-  final String Context;
+  final String title1;
+  final String title2;
+  final String context1;
+  final String context2;
+  final String? chip1Text;
+  final String? chip2Text;
+  final String? buttonText;
+  final VoidCallback? onPressed;
 
-  CustomCard({required this.Title, required this.Context});
+  CustomCard({
+    required this.title1,
+    required this.title2,
+    required this.context1,
+    required this.context2,
+    this.chip1Text,
+    this.chip2Text,
+    this.buttonText,
+    this.onPressed,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -18,19 +33,80 @@ class CustomCard extends StatelessWidget {
           children: [
             SizedBox(height: 8),
             Text(
-              '$Title',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              title1,
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 8),
             Divider(
-              color: Colors.grey,
+              color: Colors.green.shade50,
               thickness: 1,
             ),
-            SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                if (chip1Text != null)
+                  Chip(
+                    elevation: 2,
+                    label: Text(chip1Text!),
+                    backgroundColor: Colors.white,
+                    shadowColor: Colors.green.shade300,
+                    side: BorderSide(
+                      color: Colors.green.shade500, // 테두리 색상
+                      width: 1.0, // 테두리 선 두께
+                    ),
+                  ),
+                SizedBox(width: 8),
+                if (chip2Text != null)
+                  Chip(
+                    elevation: 2,
+                    label: Text(chip2Text!),
+                    backgroundColor: Colors.white,
+                    shadowColor: Colors.green.shade300,
+                    side: BorderSide(
+                      color: Colors.green.shade500, // 테두리 색상
+                      width: 1.0, // 테두리 선 두께
+                    ),
+                  ),
+              ],
+            ),
+            SizedBox(height: 20),
             Text(
-              Context,
+              "${context1}",
+              style: TextStyle(fontSize: 20),
+            ),
+            Divider(
+              color: Colors.green.shade50,
+              thickness: 1,
+            ),
+            SizedBox(height: 16),
+            Text(
+              "${title2}",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 15),
+            Text(
+              "${context2}",
               style: TextStyle(fontSize: 14),
             ),
+            SizedBox(height: 30),
+            if (buttonText != null && onPressed != null)
+              Center(
+                child: ElevatedButton(
+                  onPressed: onPressed,
+                  child: Text(buttonText!),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green.shade50, // 배경 색
+                    onPrimary: Colors.green.shade900, // 텍스트 색
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0), // 테두리 모양
+                    ),
+                    side: BorderSide(
+                      color: Colors.green.shade900, // 테두리 색상
+                      width: 1.0, // 테두리 선 두께
+                    ),
+                  ),
+                ),
+              ),
           ],
         ),
       ),
