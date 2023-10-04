7bfc411d9288e40964e1c04b75ee615b0ef90006
손한나 <sonhanna@sonhannaui-MacBookPro.local>
2023-10-05 07:01:29 +0900

    UPDATE : SafeArea 컴포넌트 추가

--- memory_forest_frontend/lib/screens/profile.dart
+++ memory_forest_frontend/lib/screens/profile.dart
@@ -16,76 +16,78 @@ class ProfileScreen extends StatelessWidget {
         title: '',
         onProfilePressed: 'null',
       ),
-      body: Container(
-        alignment: Alignment.center,
-        color: Color(0xFFB6E166),
-        child: SingleChildScrollView(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.all(0.1 * MediaQuery.of(context).size.width),
-            child: Card(
-              elevation: 4.0,
-              color: Color(0xFFFFFFFF),
-              child: Container(
-                padding: EdgeInsets.all(10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    RichText(
-                      text: TextSpan(
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          color: Colors.black, // 텍스트 색상 추가
-                        ),
-                        children: [
-                          TextSpan(
-                            text: '$name', // 변수 값 삽입
-                            style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      body: SafeArea(
+        child : Container(
+          alignment: Alignment.center,
+          color: Color(0xFFB6E166),
+          child: SingleChildScrollView(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.all(0.1 * MediaQuery.of(context).size.width),
+              child: Card(
+                elevation: 4.0,
+                color: Color(0xFFFFFFFF),
+                child: Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            color: Colors.black, // 텍스트 색상 추가
                           ),
-                          TextSpan(
-                            text: ' 님의',
-                            style: TextStyle(fontSize: 24.0),
+                          children: [
+                            TextSpan(
+                              text: '$name', // 변수 값 삽입
+                              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                            ),
+                            TextSpan(
+                              text: ' 님의',
+                              style: TextStyle(fontSize: 24.0),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10.0), // 여백 추가
+                      Text(
+                        '누적 결과 보고서입니다.',
+                        style: TextStyle(fontSize: 20.0),
+                      ),
+                      SizedBox(height: 10.0), // 여백 추가
+                      Row(
+                        children: [
+                          Container(
+                            color: Colors.green, // 녹색 배경
+                            width: MediaQuery.of(context).size.width - 120, // 화면 너비 - 120으로 수정
+                            height: 1.0, // 1px 높이
                           ),
                         ],
                       ),
-                    ),
-                    SizedBox(height: 10.0), // 여백 추가
-                    Text(
-                      '누적 결과 보고서입니다.',
-                      style: TextStyle(fontSize: 20.0),
-                    ),
-                    SizedBox(height: 10.0), // 여백 추가
-                    Row(
-                      children: [
-                        Container(
-                          color: Colors.green, // 녹색 배경
-                          width: MediaQuery.of(context).size.width - 120, // 화면 너비 - 120으로 수정
-                          height: 1.0, // 1px 높이
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 10.0), // 여백 추가
-                    // contents
-                    BarChartWidget(
-                      thisMonthCount: thisMonthCount,
-                      lastMonthCount: lastMonthCount,
-                    ),
-                    SizedBox(height: 30.0), // 여백 추가
-                    UsedWordRankingWidget(),
-                    SizedBox(height: 30.0), // 여백 추가
-                    Text(
-                      '끝말잇기 성공률',
-                      style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 10.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center, // 원을 중앙으로 정렬
-                      children: [
-                        GameSuccessRateWidget(),
-                      ],
-                    ),
-                    SizedBox(height: 30.0),
-                  ], // 이 부분 잘못된 괄호 수정
+                      SizedBox(height: 10.0), // 여백 추가
+                      // contents
+                      BarChartWidget(
+                        thisMonthCount: thisMonthCount,
+                        lastMonthCount: lastMonthCount,
+                      ),
+                      SizedBox(height: 30.0), // 여백 추가
+                      UsedWordRankingWidget(),
+                      SizedBox(height: 30.0), // 여백 추가
+                      Text(
+                        '끝말잇기 성공률',
+                        style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center, // 원을 중앙으로 정렬
+                        children: [
+                          GameSuccessRateWidget(),
+                        ],
+                      ),
+                      SizedBox(height: 30.0),
+                    ], // 이 부분 잘못된 괄호 수정
+                  ),
                 ),
               ),
             ),
