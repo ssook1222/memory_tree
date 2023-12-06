1c99eefb92b610265e77a7d787930238ebd614b4
ssook <chlthr1222@naver.com>
2023-12-06 10:15:30 +0900

    UPDATE : 데이터 불러오기 전 액션 추가 및 코드 리팩터링

--- memory_forest_frontend/lib/screens/chat_screen.dart
+++ memory_forest_frontend/lib/screens/chat_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:memory_tree_frontend/widgets/app_bar_widget.dart';
 import 'package:memory_tree_frontend/widgets/message_bubble.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:intl/intl.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
@@ -21,13 +20,12 @@ class _ChatScreenState extends State<ChatScreen> {
   final stt.SpeechToText _speech = stt.SpeechToText();
   String _transcription = '';
   final List<Map<String, String>> chatData = [];
-  bool isListening = false; // 음성 인식 중인지 여부를 나타내는 변수
-  bool isStarting = true; // 처음 시작했는 지 그 여부를 나타내는 그 변수
+  bool isListening = false;
+  bool isStarting = true;
 
   @override
   void initState() {
     super.initState();
-    // 처음 시작할 때 _sendDataToBackend1 실행
     _sendDataToBackend1();
   }
 
@@ -70,22 +68,11 @@ class _ChatScreenState extends State<ChatScreen> {
             Expanded(
               child: Container(
                 color: Color(0xFFB6E166).withOpacity(0.12),
-                child: ListView.builder(
-                  itemCount: chatData.length,
-                  itemBuilder: (context, index) {
-                    final chatItem = chatData[index];
-                    final sender = chatItem.keys.first;
-                    final message = chatItem.values.first;
-                    // MessageBubble을 누르면 음성 인식 모달을 표시하도록 수정
-                    return MessageBubble(
-                      sender: sender,
-                      message: message,
-                      onPressedCallback: () {
-                        _showListeningModal(); // 이렇게 수정
-                      },
-                    );
-                  },
-                ),
+                child: chatData.isEmpty
+                    ? Center(
+                  child: CircularProgressIndicator(),
+                )
+                    : _buildChatList(),
               ),
             ),
           ],
@@ -94,47 +81,96 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 
-  // 음성 인식 모달을 표시하는 함수
+  Widget _buildChatList() {
+    return ListView.builder(
+      itemCount: chatData.length,
+      itemBuilder: (context, index) {
+        final chatItem = chatData[index];
+        final sender = chatItem.keys.first;
+        final message = chatItem.values.first;
+        return MessageBubble(
+          sender: sender,
+          message: message,
+          onPressedCallback: _showListeningModal,
+        );
+      },
+    );
+  }
+
   void _showListeningModal() {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
-        return WillPopScope(
-          onWillPop: () async {
-            _stopListening(); // 모달 창이 닫힐 때 음성 인식 종료
-            return true;
-          },
-          child: Container(
-            padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Text(
-                  '음성 인식 중입니다.',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                // 음성 인식 종료 버튼
-                ElevatedButton(
-                  onPressed: () {
-                    _stopListening();
-                  },
-                  child: Text('음성 인식 종료'),
-                ),
-              ],
-            ),
-          ),
-        );
+        return _buildListeningModal();
       },
     );
-    // 음성 인식을 시작
     _startListening();
   }
 
-  // 음성 인식을 시작하는 함수
+  Widget _buildListeningModal() {
+    return WillPopScope(
+      onWillPop: () async {
+        _stopListening();
+        return true;
+      },
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              '음성 인식 중입니다.',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+              onPressed: _stopListening,
+              child: Text('음성 인식 종료'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _sendDataToBackend1() async {
+    final apiUrl =
+        'https://bug-free-dollop-q457q76j654367v7-8000.app.github.dev/api/gpt/start';
+    await _sendDataToBackend(apiUrl);
+  }
+
+  Future<void> _sendDataToBackend2(String data) async {
+    if (data == null || data.isEmpty) {
+      data = "안녕";
+    }
+
+    final apiUrl2 =
+        'https://bug-free-dollop-q457q76j654367v7-8000.app.github.dev/api/gpt?message=$data';
+    await _sendDataToBackend(apiUrl2);
+  }
+
+  Future<void> _sendDataToBackend(String apiUrl) async {
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final responseData = response.body;
+        final aiResponse = responseData;
+        setState(() {
+          chatData.add({'AI': aiResponse});
+        });
+        print(chatData);
+      } else {
+        print('API 호출 실패: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('API 호출 중 오류 발생: $e');
+    }
+  }
+
   void _startListening() async {
     if (await _speech.initialize()) {
       setState(() {
-        isListening = true; // 음성 인식 중인 상태로 변경
+        isListening = true;
       });
       _speech.listen(
         onResult: (result) {
@@ -148,65 +184,12 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  // 음성 인식을 중지하는 함수
   void _stopListening() {
     _speech.stop();
     setState(() {
-      isListening = false; // 음성 인식 종료 상태로 변경
+      isListening = false;
     });
-    // 음성 인식 결과를 백엔드로 전송
     _sendDataToBackend2(_transcription);
-  }
-
-  // 백엔드 API로 데이터 전송하는 함수
-  Future<void> _sendDataToBackend1() async {
-    final apiUrl =
-        'https://bug-free-dollop-q457q76j654367v7-8000.app.github.dev/api/gpt/start';
-
-    try {
-      final response = await http.get(Uri.parse(apiUrl));
-
-      if (response.statusCode == 200) {
-        // API 응답을 문자열로 변환하여 chatItem에 추가
-        final responseData = response.body;
-        final aiResponse = responseData;
-        chatData.add({'AI': aiResponse});
-    } else {
-        print('API 호출 실패: ${response.statusCode}');
-        chatData.add({'AI': 'API 호출 실패'});
-      }
-    } catch (e) {
-      print('API 호출 중 오류 발생: $e');
-      chatData.add({'AI': 'API 오류 발생'});
-    }
-  }
-
-  // 백엔드 API로 데이터 전송하는 함수
-  Future<void> _sendDataToBackend2(String data) async {
-    // 데이터가 NULL인 경우 "안녕"으로 초기화
-    if (data == null || data.isEmpty) {
-      data = "안녕";
-    }
-
-    final apiUrl2 =
-        'https://bug-free-dollop-q457q76j654367v7-8000.app.github.dev/api/gpt?message=$data';
-
-    try {
-      final response = await http.get(Uri.parse(apiUrl2));
-
-      if (response.statusCode == 200) {
-        // API 응답을 문자열로 변환하여 chatItem에 추가
-        final responseData = response.body;
-        final aiResponse = responseData;
-        chatData.add({'AI': aiResponse});
-      } else {
-        print('API 호출 실패: ${response.statusCode}');
-        chatData.add({'AI': 'API 호출 실패'});
-      }
-    } catch (e) {
-      print('API 호출 중 오류 발생: $e');
-      chatData.add({'AI': 'API 오류 발생'});
-    }
   }
 }
 
